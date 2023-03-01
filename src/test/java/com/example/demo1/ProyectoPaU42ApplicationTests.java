71d4a27edc75cc182082f49a7f7b771c6c0f0e0f
YaniryFlorez <ymflorezrivera@gmail.com>
2023-02-28 19:44:20 -0500

    controler

---
+++ src/test/java/com/example/demo1/ProyectoPaU42ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPaU42ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
